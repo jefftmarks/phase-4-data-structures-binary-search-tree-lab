833a6764bd04546b496dbe55f6f7a7a49a99aafa
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-27 22:32:06 -0400

    done with assignment

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class BinarySearchTree
   attr_accessor :root
@@ -8,11 +9,62 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+		return nil if self.root == nil
+
+    current = self.root
+		
+		while current
+			return current if current.value == value
+
+			if value < current.value
+				current = current.left
+			else
+				current = current.right
+			end
+		end
+
+		nil
+
   end
 
   def insert(value)
-    # your code here
+		new_node = Node.new(value)
+
+		if @root.nil?
+      @root = new_node
+      return @root
+    end
+
+    current = self.root
+		
+		while current
+			if value == current.value
+				return nil
+			elsif value < current.value && !current.left.nil?
+				current = current.left
+			elsif value > current.value && !current.right.nil?
+				current = current.right
+			elsif value < current.value
+				current.left = new_node
+				current = nil
+				return new_node
+			else
+				current.right = new_node
+				current = nil
+				return new_node
+			end
+		end
+
+		nil
   end
 
 end 
+
+tree = BinarySearchTree.new
+tree.root = Node.new(5)
+tree.root.left = Node.new(3)
+tree.root.right = Node.new(11)
+
+node = tree.insert(1)
+
+# binding.pry
